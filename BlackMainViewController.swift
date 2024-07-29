5d9f36f22866ae4506dd896295338a0748755fd3
Sudang <80115601+suyeon100@users.noreply.github.com>
2024-07-29 23:54:04 +0900

    moreAction add

--- BlackMainViewController.swift
+++ BlackMainViewController.swift
@@ -19,8 +19,10 @@ class BlackMainViewController: UIViewController {
         // Do any additional setup after loading the view.
         
        blackTableView.register(UINib(nibName: "FirstTableViewCell", bundle: nil), forCellReuseIdentifier: "FirstTable" )
+       blackTableView.register(UINib(nibName: "SecondTableViewCell", bundle: nil), forCellReuseIdentifier: "SecondTable" )
         blackTableView.dataSource = self
         blackTableView.delegate = self
+        
     }
 
     
@@ -32,17 +34,47 @@ extension BlackMainViewController: UITableViewDataSource, UITableViewDelegate
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 3
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FirstTable", for: indexPath) as! FirstTableViewCell
         
-        return cell
+        switch indexPath.row{
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FirstTable", for: indexPath) as! FirstTableViewCell
+            
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SecondTable", for: indexPath) as! SecondTableViewCell
+            
+            cell.addMoreClickedHandler = {
+                guard let nextVC = self.storyboard?.instantiateViewController(identifier: "webView") else {return}
+              
+                self.present(nextVC, animated: true, completion: nil)
+            }
+            
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FirstTable", for: indexPath) as! FirstTableViewCell
+            
+            return cell
+            
+        default:
+            return UITableViewCell()
+        }
     }
 //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-     return 250
+        switch indexPath.row{
+        case 0:
+            return 200
+        case 1:
+            return 50
+        case 2:
+            return 200
+        default:
+            return 0
+        }
     }
     
 }
---
+++ WebViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WebViewController.swift
+//  Black
+//
+//  Created by 박수연 on 7/23/24.
+//
+
+import UIKit
+
+
+class WebViewController: UIViewController {
+  
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+        // Do any additional setup after loading the view.
+    }
+
+ 
+
+}
