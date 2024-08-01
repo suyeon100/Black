1eca62c1d7af3618bf42fb5004a9093392f13c42
Sudang <80115601+suyeon100@users.noreply.github.com>
2024-08-01 23:24:00 +0900

    TabBar add

---
+++ BlackTwoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BlackTwoViewController.swift
+//  Black
+//
+//  Created by 박수연 on 8/1/24.
+//
+
+import UIKit
+
+class BlackTwoViewController: UIViewController {
+    
+    //MARK:- 두번쨰 탭 / 추가기능 :  1. 양옆으로 스크롤 컬렉션뷰, 시간초로 넘어가는 기능 timer 추가 
+    
+    
+    @IBOutlet weak var twotableView: UITableView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
